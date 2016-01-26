e845bab1b7759d17b799b292fcaae9d058118979
Cyber-Shadow <hydrahead3355@outlook.com>
2016-01-26 20:34:29 +1100

    Update SAAI.py

--- SAAI.py
+++ SAAI.py
@@ -1,7 +1,15 @@
+#Import Modules
 import pyttsx
 from random import randint
+import datetime
 
+#Variables
 engine = pyttsx.init()
+
+now = datetime.datetime.now()
+
+birthdate = datetime.datetime(2016, 01, 26, 11, 37, 00)
+age = now - birthdate
 
 def choice2(str, str2):
     random = randint(1, 2)
@@ -42,16 +50,21 @@ while True:
     userInput = raw_input(">>> ").lower()
     userInput = ''.join([c for c in userInput if c not in ('!', '?', '.', ',')])
     if userInput in ['hi', 'hello']:
-        choice("Hello!", "Hi there!")
+        choice2("Hello!", "Hi there!")
     elif userInput in ['hi saai', 'hello saai']:
-        choice("Hello to you too.", "You know my name!")
+        choice2("Hello to you too.", "You know my name!")
     elif userInput in ['tell me a joke', "tell me something funny"]:
         choice5("NO", "What do you call a blonde who has dyed her hair brown? ... Artificial intelligence!",
                 "I had to sell my first vacuum cleaner ... It sucked.",
                 "Why did the platypus cross the road? ... Because it was in the wrong joke.",
                 "How does the moon cut his hair? ... Eclipse it." )
+    elif userInput in ['age']:
+        print (age)
+        engine.say("I am")
+        engine.say(age)
+        engine.say("old")
+        engine.runAndWait()
     else:
         print("I don't understand.")
         engine.say("I don't understand.")
         engine.runAndWait()
-
