110abae2bf9764f5e34cb29d760ff05684ac3595
Derp McDerpson <DerpfacePython@users.noreply.github.com>
2016-01-26 16:06:55 +1100

    Added a function.

--- SAAI.py
+++ SAAI.py
@@ -1,11 +1,25 @@
 import pyttsx
+from random import randint
+
 engine = pyttsx.init()
+
+def choice(str, str2):
+    random = randint(1, 2)
+    if random == 1:
+        print(str)
+        engine.say(str)
+        engine.runAndWait()
+    else:
+        print(str2)
+        engine.say(str2)
+        engine.runAndWait()
+
 while True:
     userInput = raw_input(">>> ").lower()
     if userInput in ['hi', 'hello']:
-        print("Hello")
-        engine.say("hello")
+        choice("Hello!", "Hi there!")
+    else:
+        print("I don't understand.")
+        engine.say("I don't understand.")
         engine.runAndWait()
 
-    else:
-        print("I did not understand what you said")
