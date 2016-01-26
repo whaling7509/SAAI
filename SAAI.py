f87c0a1fc649a721da2a0ac4af7901c36ddd2006
Cyber-Shadow <hydrahead3355@outlook.com>
2016-01-26 12:03:12 +1100

    Added Speech Engine

--- SAAI.py
+++ SAAI.py
@@ -1,7 +1,11 @@
+import pyttsx
+engine = pyttsx.init()
 while True:
     userInput = raw_input(">>> ").lower()
     if userInput in ['hi', 'hello']:
         print("Hello")
-        os.system("say 'hello'")
+        engine.say("hello")
+        engine.runAndWait()
+
     else:
         print("I did not understand what you said")
