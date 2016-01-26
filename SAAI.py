bc33f12a8a94e3ffc17d3d7c90f90150aa9f9f16
Cyber-Shadow <hydrahead3355@outlook.com>
2016-01-26 16:57:38 +1100

    Update SAAI.py

--- SAAI.py
+++ SAAI.py
@@ -3,7 +3,7 @@ from random import randint
 
 engine = pyttsx.init()
 
-def choice(str, str2):
+def choice2(str, str2):
     random = randint(1, 2)
     if random == 1:
         print(str)
@@ -13,6 +13,30 @@ def choice(str, str2):
         print(str2)
         engine.say(str2)
         engine.runAndWait()
+    
+    
+def choice5(str, str2, str3, str4, str5):
+    random = randint(1, 5)
+    if random == 1:
+        print(str)
+        engine.say(str)
+        engine.runAndWait()
+    elif random == 2:
+        print(str2)
+        engine.say(str2)
+        engine.runAndWait()
+    elif random == 3:
+        print(str3)
+        engine.say(str3)
+        engine.runAndWait()
+    elif random == 4:
+        print(str4)
+        engine.say(str4)
+        engine.runAndWait()
+    else:
+        print(str5)
+        engine.say(str5)
+        engine.runAndWait()
 
 while True:
     userInput = raw_input(">>> ").lower()
@@ -21,6 +45,11 @@ while True:
         choice("Hello!", "Hi there!")
     elif userInput in ['hi saai', 'hello saai']:
         choice("Hello to you too.", "You know my name!")
+    elif userInput in ['tell me a joke', "tell me something funny"]:
+        choice5("NO", "What do you call a blonde who has dyed her hair brown? ... Artificial intelligence!",
+                "I had to sell my first vacuum cleaner ... It sucked.",
+                "Why did the platypus cross the road? ... Because it was in the wrong joke.",
+                "How does the moon cut his hair? ... Eclipse it." )
     else:
         print("I don't understand.")
         engine.say("I don't understand.")
