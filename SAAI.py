b354f86e0fa2bc0da3bc8c43acb321027c7754cb
Cyber-Shadow <hydrahead3355@outlook.com>
2016-02-13 22:04:53 +1100

    Update SAAI.py

--- SAAI.py
+++ SAAI.py
@@ -3,10 +3,11 @@ import pyttsx
 from random import randint
 import datetime
 from chatterbot import ChatBot
+import yweather
 
 #Variables and commands
 engine = pyttsx.init()
-
+client = yweather.Client()
 
 bot = ChatBot("No Output",
     storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
@@ -22,42 +23,13 @@ birthdate = datetime.datetime(2016, 01, 26, 11, 37, 00)
 age = now - birthdate
 
 
+class choice:
+    def __init__(self, data):
+        self.data = data
+    def call(self):
+        random = randint(1, len(data))
+        return data[random - 1]
 
-
-def choice2(str, str2):
-    random = randint(1, 2)
-    if random == 1:
-        print(str)
-        engine.say(str)
-        engine.runAndWait()
-    else:
-        print(str2)
-        engine.say(str2)
-        engine.runAndWait()
-
-
-def choice5(str, str2, str3, str4, str5):
-    random = randint(1, 5)
-    if random == 1:
-        print(str)
-        engine.say(str)
-        engine.runAndWait()
-    elif random == 2:
-        print(str2)
-        engine.say(str2)
-        engine.runAndWait()
-    elif random == 3:
-        print(str3)
-        engine.say(str3)
-        engine.runAndWait()
-    elif random == 4:
-        print(str4)
-        engine.say(str4)
-        engine.runAndWait()
-    else:
-        print(str5)
-        engine.say(str5)
-        engine.runAndWait()
 
 while True:
 
@@ -72,26 +44,27 @@ while True:
                    " ... You will find my page at: github.com/Cyber-Shadow/SAAI")
         engine.runAndWait()
     elif "weather" in userInput:
-        print "I have no idea, my Creator was to lazy to program a weather function. " \
-              "I do have a tip for you though, look or even go outside," \
-              " this way you can feel heat and cold and see the rain" \
-              "... When you're finished with that tell me about it."
-        engine.say("I have no idea, my Creator was to lazy to program a weather function ..."
-                   " but I do have a tip for you though ..."
-                   "look or even go outside, this way you can feel heat and cold and see the rain ..."
-                   " When you're finished with that tell me about it.")
+        weathersyd = client.fetch_weather("1105779", metric=True)
+        w1 = weathersyd["condition"]["text"]
+        w2 = weathersyd["condition"]["temp"]
+
+        print ("It's currently " + w1 + " and is " + w2 + " degree celsius. ")
+        engine.say ("It's currently" + w1 + " and is " + w2 + "degree celsius. ")
         engine.runAndWait()
+
     elif userInput in ['hi saai', 'hello saai']:
-        choice2("Hello to you too.", "You know my name!")
+        data = ["Hello to you too.", "You know my name!"]
+        x = choice(data)
+        print(x.call())
     elif userInput in ['tell me a joke', "tell me something funny"]:
-        choice5("NO", "What do you call a blonde who has dyed her hair brown? ... Artificial intelligence!",
+        data = ["NO", "What do you call a blonde who has dyed her hair brown? ... Artificial intelligence!",
                 "I had to sell my first vacuum cleaner ... It sucked.",
                 "Why did the platypus cross the road? ... Because it was in the wrong joke.",
-                "How does the moon cut his hair? ... Eclipse it." )
+                "How does the d cut his hair? ... Eclipse it."]
+        x = choice(data)
+        print(x.call())
     elif userInput in ['what is your age', 'how old are you']:
-        print "I am"
-        print (age)
-        print "old, since you launched me"
+        print ("I am " + str(age) + "old, since you launched me")
         engine.say("I am")
         engine.say(age)
         engine.say("old, since you launched me")
@@ -101,4 +74,4 @@ while True:
         bot.train("chatterbot.corpus.english.conversations")
         engine.say(botoutput)
         engine.runAndWait()
-    print ""
+
