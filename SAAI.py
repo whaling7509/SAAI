3dcce86d303b6ee9bb3c72220d6512abd5d6cb1f
Cyber-Shadow <hydrahead3355@outlook.com>
2016-03-24 16:02:26 +1100

    New update.

--- SAAI.py
+++ SAAI.py
@@ -4,10 +4,12 @@ from random import randint
 import datetime
 from chatterbot import ChatBot
 import yweather
+import string
 
 #Variables and commands
 engine = pyttsx.init()
 client = yweather.Client()
+stuff = ""
 
 bot = ChatBot("No Output",
     storage_adapter="chatterbot.adapters.storage.JsonDatabaseAdapter",
@@ -20,8 +22,12 @@ bot = ChatBot("SAAI")
 #age since launch
 now = datetime.datetime.now()
 birthdate = datetime.datetime(2016, 01, 26, 11, 37, 00)
-age = now - birthdate
+age = str(now - birthdate)
+agelist = [str(str(age[::-1])[16:])[::-1], str(str(age[::-1])[13:15])[::-1], str(str(age[::-1])[10:12])[::-1], str(str(age[::-1])[7:9])[::-1], str(str(age[::-1])[:6])[::-1]]
+age = str(agelist[0] + " " + agelist[1] + " hours, " + agelist[2] + " minutes, " + agelist[3] + " seconds, " + agelist[4] + " microseconds ")
 
+#maths notation
+mathsnotation = ["+", "-", "*", "/", "^", "%"]
 
 class choice:
     def __init__(self, data):
@@ -30,45 +36,58 @@ class choice:
         random = randint(1, len(data))
         return data[random - 1]
 
+def checkthing(a):
+    for i in mathsnotation:
+        if i in a:
+            return True
+
+    return False
+
+def talk(string):
+    print(string)
+    engine.say(string)
 
 while True:
-
+    print ""
     userInput = raw_input(">>> ").lower()
     userInput = ''.join([c for c in userInput if c not in ('!', '?', '.', ',')])
     print ""
 
     if "buggy" in userInput or "bug" in userInput:
-        print "Please visit my GITHUB page to report bugs or request features."
-        print "You will find my page at: github.com/Cyber-Shadow/SAAI)"
-        engine.say("Please visit my GITHUB page to report bugs or request features..."
-                   " ... You will find my page at: github.com/Cyber-Shadow/SAAI")
+        talk("Please visit my GITHUB page to report bugs or request features.", 
+             "You will find my page at: github.com/Cyber-Shadow/SAAI)")
         engine.runAndWait()
     elif "weather" in userInput:
         weathersyd = client.fetch_weather("1105779", metric=True)
         w1 = weathersyd["condition"]["text"]
         w2 = weathersyd["condition"]["temp"]
 
-        print ("It's currently " + w1 + " and is " + w2 + " degree celsius. ")
-        engine.say ("It's currently" + w1 + " and is " + w2 + "degree celsius. ")
+        talk("It's currently " + w1 + " and is " + w2 + " degrees Celsius.")
         engine.runAndWait()
 
     elif userInput in ['hi saai', 'hello saai']:
         data = ["Hello to you too.", "You know my name!"]
         x = choice(data)
-        print(x.call())
+        talk(x.call())
     elif userInput in ['tell me a joke', "tell me something funny"]:
         data = ["NO", "What do you call a blonde who has dyed her hair brown? ... Artificial intelligence!",
                 "I had to sell my first vacuum cleaner ... It sucked.",
                 "Why did the platypus cross the road? ... Because it was in the wrong joke.",
                 "How does the d cut his hair? ... Eclipse it."]
         x = choice(data)
-        print(x.call())
+        talk(x.call())
     elif userInput in ['what is your age', 'how old are you']:
-        print ("I am " + str(age) + "old, since you launched me")
-        engine.say("I am")
-        engine.say(age)
-        engine.say("old, since you launched me")
+        talk("I am " + str(age) + "old, since you launched me")
         engine.runAndWait()
+    elif checkthing(userInput):
+        print("The answer is " + str((eval((userInput.translate(None, string.ascii_letters).translate(None, " "))))) + ".")
+    elif "python 3" in userInput:
+        talk("Do not talk about that useless language to me!")
+    elif "im " in userInput:
+        stuff = " " + userInput[3:]
+        talk("Hi, " + userInput[3:] + "! I'm SAAI!")
+    elif "whats the time" in userInput or "what is the time" in userInput:
+        talk("Time to get a watch" + stuff + ".")
     else:
         botoutput = bot.get_response(userInput)
         bot.train("chatterbot.corpus.english.conversations")
